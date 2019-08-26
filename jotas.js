4bdb48b400ae920f2b10ca75c37b80e832d776c1
Lucas Gabriel Mofardini de Almeida <48898538+LucasMofardini@users.noreply.github.com>
2019-08-26 12:23:02 -0300

    Add files via upload

---
+++ jotas.js
@@ -0,0 +1,20 @@
+
+function preto() {
+	// alert("belezas");
+	document.getElementById('bodi').style.backgroundColor = 'black';
+	document.getElementById('bodi').style.color = 'white';
+	document.getElementsByTagName('h2').style.color = 'white';
+	document.getElementsByTagName('h1').style.color = 'white';
+	document.getElementById('titulo').style.color = 'white';
+
+}
+function branco() {
+	// alert("belezas");
+	document.getElementById('bodi').style.backgroundColor = 'white';
+	document.getElementById('bodi').style.color = 'black';
+	document.getElementsByTagName('h2').style.color = 'black';
+	document.getElementsByTagName('h1').style.color = 'black';
+
+	document.getElementById('titulo').style.color = 'black';
+
+}
